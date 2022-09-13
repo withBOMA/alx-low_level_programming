b1a2ce455e23634c46e1eaa680262097c7750ea5
withBOMA <100360948+withBOMA@users.noreply.github.com>
2022-09-13 22:44:14 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,11 @@
 
 int main(void)
 {
-char c[10] = "_putchar";
-int i = 0;
-while (i < 9)
+char c[] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
 	{
 	_putchar(c[i]);
-	i++;
 	}
 _putchar('\n');
 return (0);
