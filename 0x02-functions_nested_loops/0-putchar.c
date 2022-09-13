211be191f23fe1604f1fd1674d4a69d1c77cbdb0
Adeyanju <adeyanjuboluwaji@gmail.com>
2022-09-13 11:47:19 -0700

    0 added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "putchar.h"
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+
+int main(void)
+{
+char c[10] = "putchar"
+int i = 0;
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
