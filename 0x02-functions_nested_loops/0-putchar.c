1a3782fd9cc33aeba2921ce03ce8460c13b9218e
Adeyanju <adeyanjuboluwaji@gmail.com>
2022-09-13 12:29:32 -0700

    adjust

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,14 @@
 
 int main(void)
 {
-char c[10] = "_putchar";
-int i = 0;
-while (i < 9)
-{
-_putchar(c[i]);
-i++;
-}
-_putchar('\n');
-return (0);
+	char c[10] = "putchar";
+
+	int i = 0;
+	while (i < 9)
+		{
+			_putchar(c[i]);
+			i++;
+		}
+	_putchar('\n');
+	return (0);
 }
