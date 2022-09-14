5dfd1cbeab1cf256524dddee97e779789ecfb9bf
withBOMA <100360948+withBOMA@users.noreply.github.com>
2022-09-14 02:18:39 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,42 +2,25 @@
 #include <stdio.h>
 
 /**
-
  * main - Entry point
-
  *
-
  * Description: Prints the sum of even-valued
-
  *		Fibonacci sequence not exceed
-
  *		4million
-
  *
-
  * Return: Always 0 (Success)
-
-*/
-
-
+ */
 
 int main(void)
-
 {
-
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
 	float total_sum;
 
-
-
 	while (1)
 
 	{
-
 		sum = fib1 + fib2;
-
-
 
 		if (sum > 4000000)
 
