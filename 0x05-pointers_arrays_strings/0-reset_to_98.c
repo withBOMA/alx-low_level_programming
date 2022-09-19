5ccfe975cfdf834ec1d20cefd1191ad3beaaf65b
withBOMA <100360948+withBOMA@users.noreply.github.com>
2022-09-19 21:02:25 +0100

    Create 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,20 +1,13 @@
-et_to_98 - takes a pointer to an int parameter
-
+/**
+ * reset_to_98 - takes a pointer to an int parameter
  *               and updates the value it points to 98
  *
- *                *
+ * @n: int parameter
  *
- *                 * @n: int parameter
- *
- *                  *
- *
- *                   * Return: Nothing
- *
- *                    */
+ * Return: Nothing
+ */
 
-
-
- void reset_to_98(int *n)
+void reset_to_98(int *n)
 {
-		*n = 98;
+	*n = 98;
 }
