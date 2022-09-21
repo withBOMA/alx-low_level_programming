7155e9664cfef1933a3a8942fa095927716257d9
Emmanuel Adeyanju <100360948+withBOMA@users.noreply.github.com>
2022-09-21 20:07:51 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,37 @@
-#include "main.h"
-
 /**
-* leet - function that encode a string
-*@str:string that will be encoded
-*Return:returns encoded string
+ * leet - a function that encodes a string into 1337
+ *
+ * @s: string input
+ *
+ * Return: @s
 */
 
-char *leet(char *str)
+char *leet(char *s)
 {
-	int index1 = 0, index2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int i, c = 0;
+	int sl[] = {97, 101, 111, 116, 108};
+	int ul[] = {65, 69, 79, 84, 76};
+	int n[] = {52, 51, 48, 55, 49};
 
-	while (str[++index1])
+	/*iterate values in array s*/
+	while (s[c] != '\0')
 	{
-		for (index2 = 0; index2 <= 7; index2++)
+		/**
+		 * loop through array value five times
+		 * to check if the value is equal to
+		 * any small letter in array sl or
+		 * upper letter in array ul and if it
+		 * is replace it with the value in array n
+		*/
+		for (i = 0; i < 5; i++)
 		{
-			if (str[index1] == leet[index2] ||
-			 str[index1] - 32 == leet[index2])
-				str[index1] = index2 + '0';
+			if (s[c] == sl[i] || s[c] == ul[i])
+			{
+				s[c] = n[i];
+				break;
+			}
 		}
+		c++;
 	}
-	return (str);
+	return (s);
 }
