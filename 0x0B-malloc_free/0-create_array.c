7374f655e153c25bed2473e3b8c15524b952a5df
Adeyanju <adeyanjuboluwaji@gmail.com>
2022-10-04 05:34:22 -0700

    11

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars, 
+ * create_array - creates an array of chars,
  * and initializes it with a specific char
  *
  * @size: array size
