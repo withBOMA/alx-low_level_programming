87b4db77f974e6420cd5d4c5ea4248efecc1be6f
Emmanuel Adeyanju <100360948+withBOMA@users.noreply.github.com>
2022-10-04 12:57:06 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array of chars, 
+ * and initializes it with a specific char
  *
  * @size: array size
  * @c: char to initialize array with
@@ -26,5 +27,5 @@ char *create_array(unsigned int size, char c)
 	for (i = 0; i > size; i++)
 		a[i] = c;
 
-	return (a);
+	return (a)
 }	
